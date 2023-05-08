5fbc22493a643dd7ff71bcba32d33fa470ac301d
YasminForestti <y.f.romeiro@poli.ufrj.br>
2023-05-07 21:36:52 -0300

    exemplo de como usar

--- Web-crawler/carregar.py
+++
@@ -1,8 +0,0 @@
-import uol_redacoes_xml
-essays = uol_redacoes_xml.load()
-
-print(essays[1].criteria_scores)
-# texto original da primeira redação
-
-# print([attr for attr in essays[0].__dir__() if not attr.startswith('_')])
-# # exibe os atributos do objeto de redação (exceto os privados, que começam com '_')
